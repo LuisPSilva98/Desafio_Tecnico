01f658094a6610073fd4d3564c707e8f2decd937
unknown <luisp.silva1998@gmail.com>
2025-05-15 17:36:24 -0300

    Projeto Spring Boot inicial

---
+++ api_consulta_credito/src/main/java/com/desafio/api_consulta_credito/ApiConsultaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.api_consulta_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaCreditoApplication.class, args);
+	}
+
+}
